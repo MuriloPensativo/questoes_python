e9945ac8db540fed25d51ef8b1fc3b498d433972
Murilo Rodrigues Maia <murilo.maia62@aluno.ifce.edu.br>
2023-08-13 17:18:24 -0300

    questão 4 feita

--- questao4.py
+++ questao4.py
@@ -5,14 +5,23 @@ import unittest
 # Então, [1, 2, 2, 3] retornará [1, 2, 3]
 # Você pode criar uma nova lista ou modificar a lista atual.
 def remove_adjacent(nums):
-    pass
+  index = 0
+  while True:
+    if index >= len(nums) - 1 or len(nums) <= 1:
+      return nums
+    elif nums[index] == nums[index + 1]:
+      nums.pop(index + 1)
+    else:
+      index += 1
 
 # Dado duas listas ordenadas em ordem crescente, criar e retornar uma
 # lista de todos os elementos em ordem algabética.
 # Você pode modificar as listas passadas.
 # Idealmente, a solução deve trabalhar em tempo "linear", que passa uma única vez em ambas as listas.
 def linear_merge(list1, list2):
-    pass
+  list3 = list1 + list2
+  list3.sort()
+  return list3
 
 class MyTest(unittest.TestCase):
   def test_remove_adjacent(self):
